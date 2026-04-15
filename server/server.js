32aaf8622184d498f22f8711f763975e5658f275
zoe.mwangi <projectswithzoe@gmail.com>
2026-04-15 11:00:44 +0100

    server.js modified to work on VPS

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 import 'dotenv/config'
 import { buildApp } from './app.js'
 
-const app = await buildApp({ logger: true })
+const start = async()=>{
+  try {
+    const app = await buildApp({ logger: true })
 
 await app.listen({
   port: Number(process.env.PORT ?? 3001),
   host: '0.0.0.0'
 })
+
+console.log(`Server running on port ${process.env.PORT}`)
+  } catch (err){
+    console.error('Server failed to start')
+    process.exit(1)
+
+  }
+}
+
+start()
+
