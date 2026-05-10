53d16e414d361b290cebc9a6381223af989805bf
zoe.mwangi <projectswithzoe@gmail.com>
2026-05-11 00:32:22 +0100

    added sentry for error tracking

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as Sentry from "@sentry/react";
 import { Toaster } from 'react-hot-toast'
 import { Analytics } from '@vercel/analytics/react'
 import { AuthProvider }    from './context/AuthContext.jsx'
 import { ErrorBoundary }   from './components/ErrorBoundary.jsx'
 import App from './App.jsx'
 import './index.css'
+
+Sentry.init({
+  dsn: "https://1a7426000cd43e3e61f082269129d527@o4511071798951936.ingest.de.sentry.io/4511263769886800",
+  sendDefaultPii: true,
+  environment: "production"
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
